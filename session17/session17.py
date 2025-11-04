9dae889ed7ab8545e8d223c628219d4a67a8d22f
sali mft <thesalimft@gmail.com>
2025-11-04 17:53:10 +0330

    add session 17, part 1

---
+++ session17/session17.py
@@ -0,0 +1,25 @@
+# r ==> read
+# w ==> write
+# a ==> append
+
+
+# write to file
+# f = open('test.txt', 'w')
+# f.write('hello')
+# f.close()
+
+
+
+# read a file
+# f = open('test.txt', 'r')
+# print(f.readlines())
+# f.close()
+
+
+
+# open file and close automatically
+# with open('test.txt', 'r') as f:
+#     print(f.readline())
+#     print(f.readline())
+    
+
