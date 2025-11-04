0e3e70e9fb77149ef2cf407b6f5f91a95b2e8023
sali mft <thesalimft@gmail.com>
2025-11-04 18:54:47 +0330

    add json to session 17

--- session17/project02/main.py
+++ session17/project02/main.py
@@ -1,11 +1,11 @@
+import json
+
 students = {
     '103': {'name': 'ali', 'score': {'math': 20, 'chimi': 19}},
     '104': {'name': 'reza', 'score': {'math': 18}},
     '105': {'name': 'sara', 'score': {'chimi': 17}},
 }
 
-with open('test.txt', 'w') as f:
-    f.write(students)
+with open('data.json', 'w') as f:
+    json.dump(students, f)
     
-    
-json
